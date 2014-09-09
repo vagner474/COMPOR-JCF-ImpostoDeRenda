495c97dcf0356d8fd1fb4124f45f798a6da31d61
Vagner Silva <vagner.silva@dce.ufpb.br>
2014-09-09 20:56:00 -0300

    T.01.04 finalizado

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
 import br.ufcg.ppgcc.compor.ir.Titular;
+import br.ufcg.ppgcc.compor.ir.ExcecaoImpostoDeRenda;
 
 public class ImpostoDeRenda implements FachadaExperimento {
 
 	private List<Titular> titulares = new ArrayList<Titular>();
 	
 		public void criarNovoTitular(Titular titular) {
+			if(titular.getNome() == null){
+				throw new ExcecaoImpostoDeRenda("O campo Nome é obrigatório");
+			}
 			titulares.add(titular);	
 		}
 		public List<Titular> listarTitulares() {
