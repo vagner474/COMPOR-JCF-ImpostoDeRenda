c6cb3a5e58e18298c9a25a7a8039d73febcf32a3
Vagner Silva <vagner.silva@dce.ufpb.br>
2014-09-09 22:32:08 -0300

    T.04.01 finalizado

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -9,6 +9,7 @@ import br.ufcg.ppgcc.compor.ir.Titular;
 import br.ufcg.ppgcc.compor.ir.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.FontePagadora;
 import br.ufcg.ppgcc.compor.ir.Dependente;
+import br.ufcg.ppgcc.compor.ir.Resultado;
 
 public class ImpostoDeRenda implements FachadaExperimento {
 
@@ -31,12 +32,10 @@ public class ImpostoDeRenda implements FachadaExperimento {
 		public List<Titular> listarTitulares() {
 			return new ArrayList<Titular>(historicoTitularFonte.keySet());
 		}
-	
 		public void criarFontePagadora(Titular titular, FontePagadora fonte) {
 			if (fonte.getNome() == null) {
 				throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");
 			}
-	
 			if (fonte.getRendimentoRecebidos() == 0.0) {
 				throw new ExcecaoImpostoDeRenda(
 					"O campo rendimentos recebidos é obrigatório");
@@ -62,11 +61,34 @@ public class ImpostoDeRenda implements FachadaExperimento {
 			return historicoTitularFonte.get(titular);
 		}
 		public void criarDependente(Titular titular, Dependente dependente) {
-			ArrayList<Dependente> dependentesDoTitular = (ArrayList<Dependente>) dependentes.get(titular);
+			if (dependente.getCpf() == null) {
+				throw new ExcecaoImpostoDeRenda("O campo CPF é obrigatório");
+				}
+			if (dependente.getNome() == null) {
+				throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");
+			}
+			if (dependente.getTipo() == 0) {
+				throw new ExcecaoImpostoDeRenda("O campo tipo é obrigatório");
+			}
+			if (dependente.getTipo() < 0) {
+				throw new ExcecaoImpostoDeRenda("O campo tipo é inválido");
+			}
+			if (!dependente.getCpf().matches("\\d{3}.\\d{3}.\\d{3}-\\d{2}")) {
+				throw new ExcecaoImpostoDeRenda("O campo CPF é inválido");
+			}
+			if (dependentes.containsKey(titular)) {
+				ArrayList<Dependente> dependentesDoTitular = (ArrayList<Dependente>) dependentes.get(titular);
 				dependentesDoTitular.add(dependente);
 				dependentes.put(titular, dependentesDoTitular);
+			}
+			else {
+				throw new ExcecaoImpostoDeRenda("Titular não cadastrado");
+			}
 		}
 		public List<Dependente> listarDependentes(Titular titular) {
 			return new ArrayList<Dependente>(dependentes.get(titular));
+		}
+		public Resultado declaracaoCompleta(Titular titular) {
+			return new Resultado();
 		}
 }
